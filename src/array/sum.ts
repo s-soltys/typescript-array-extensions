83e333a16ada6a37b4877c980b19045ab2a446fc
jimmytheneutrino <aleksei.lissitsin@gmail.com>
2017-11-21 19:00:35 +0200

    fix sum, see #1

--- src/array/sum.ts
+++ src/array/sum.ts
@@ -5,12 +5,9 @@ interface Array<T> {
 
 ((proto: any) => {
     if (typeof proto.sum === "function") return;
-    
+
     proto.sum = function sum<T>(this: Array<T>, callbackFn: (value: T, index: number, array: T[]) => number): number {
-        if (typeof callbackFn === 'function') {
-            return this.reduce((sum, value, currentIndex, array) => sum + callbackFn(value, currentIndex, array), 0);
-        } else {
-            return this.reduce((sum, value) => sum + parseInt(value as any), 0);
-        }
+        const callback = (typeof callbackFn === 'function') ? callbackFn : x => Number(x);
+        return this.reduce((sum, value, currentIndex, array) => sum + callback(value, currentIndex, array), 0);
     }
 })(Array.prototype);
